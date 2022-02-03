0aa120d6c254a6b641134335ee45d549cdf8d00c
Kevin Firko <kfirko@bitcurve.com>
2022-02-02 21:10:51 -0500

    initial commit - aws-cdk v2

--- bin/fx-cdk-recipes.ts
+++ bin/fx-cdk-recipes.ts
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { FxCdkRecipesStack } from '../lib/fx-cdk-recipes-stack';
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { FxCdkStack } from '../lib/fx-cdk-stack'
 
-const app = new cdk.App();
-new FxCdkRecipesStack(app, 'FxCdkRecipesStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const app = new cdk.App()
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+const env = {
+  account: app.node.tryGetContext('region') || process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+  region: app.node.tryGetContext('account') || process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new FxCdkStack(app, 'FxCdkStack', {
+  env,
+})
